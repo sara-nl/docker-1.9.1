0e982dcd02a5f92fb23496f8fe34a318628f0f1f
Aasmund Eldhuset <aeldhuset@medallia.com>
2015-07-21 16:21:23 -0700

    Mapping and unmapping Ceph.

--- volume/ceph/ceph.go
+++ volume/ceph/ceph.go
@@ -6,6 +6,11 @@ import (
 	"runtime/debug"
 
 	"github.com/docker/docker/volume"
+	"github.com/Sirupsen/logrus"
+	"os/exec"
+	"bytes"
+	"strings"
+	"fmt"
 )
 
 func New() *Root {
@@ -31,10 +36,25 @@ func (r *Root) Create(name string) (volume.Volume, error) {
 
 	v, exists := r.volumes[name]
 	if !exists {
-		//TODO: Ceph map
+		//TODO: Might want to map with --options rw/ro here, but then we need to sneak in the RW flag somehow
+		cmd := exec.Command("rbd", "map", name)
+		var stdout bytes.Buffer
+		var stderr bytes.Buffer
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
+		var mappedDevicePath string
+		if err := cmd.Run(); err == nil {
+			mappedDevicePath = strings.TrimRight(stdout.String(), "\n")
+			logrus.Infof("Succeeded in mapping Ceph volume %s to %s", name, mappedDevicePath)
+		} else {
+			msg := fmt.Sprintf("Failed to map Ceph volume %s: %s - %s", name, err, strings.TrimRight(stderr.String(), "\n"))
+			logrus.Errorf(msg)
+			return nil, errors.New(msg)
+		}
 		v = &Volume{
-			driverName: r.Name(),
-			name:       name,
+			driverName:       r.Name(),
+			name:             name,
+			mappedDevicePath: mappedDevicePath,
 		}
 		r.volumes[name] = v
 	}
@@ -53,7 +73,14 @@ func (r *Root) Remove(v volume.Volume) error {
 	}
 	lv.release()
 	if lv.usedCount == 0 {
-		//TODO: Ceph unmap
+		cmd := exec.Command("rbd", "unmap", lv.mappedDevicePath)
+		var stderr bytes.Buffer
+		cmd.Stderr = &stderr
+		if err := cmd.Run(); err == nil {
+			logrus.Infof("Succeeded in unmapping Ceph volume %s from %s", lv.name, lv.mappedDevicePath)
+		} else {
+			logrus.Errorf("Failed to unmap Ceph volume %s from %s: %s - %s", lv.name, lv.mappedDevicePath, err, strings.TrimRight(stderr.String(), "\n"))
+		}
 		delete(r.volumes, lv.name)
 	}
 	return nil
@@ -66,6 +93,8 @@ type Volume struct {
 	name string
 	// driverName is the name of the driver that created the volume.
 	driverName string
+	// the path to the device to which the Ceph volume has been mapped
+	mappedDevicePath string
 }
 
 func (v *Volume) Name() string {
@@ -85,7 +114,7 @@ func (v *Volume) Path() string {
 
 func (v *Volume) Mount() (string, error) {
 	debug.PrintStack()
-	return "", nil
+	return v.mappedDevicePath, nil
 }
 
 func (v *Volume) Unmount() error {
